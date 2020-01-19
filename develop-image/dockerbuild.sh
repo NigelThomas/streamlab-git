598f4a8b70ae32fb10cf15b3cfbe7ced8b1b3271
NigelThomas <nigel.cl.thomas@gmail.com>
2020-01-19 16:30:06 +0000

    carry through the image tag from the source images

--- develop-image/dockerbuild.sh
+++ develop-image/dockerbuild.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker build --pull=true -t sqlstream/streamlab-git-dev .
-docker push sqlstream/streamlab-git-dev
+: ${BASE_IMAGE_LABEL:=release}
+export BASE_IMAGE_LABEL
+docker build --pull=true --build-arg BASE_IMAGE_LABEL=$BASE_IMAGE_LABEL -t sqlstream/streamlab-git-dev:$BASE_IMAGE_LABEL .
+docker push sqlstream/streamlab-git-dev:${BASE_IMAGE_LABEL}
--- production-image/dockerbuild.sh
+++ production-image/dockerbuild.sh
@@ -1,3 +1,5 @@
 #¡/bin/bash
-docker build --pull=true -t sqlstream/streamlab-git .
-docker push sqlstream/streamlab-git
+: ${BASE_IMAGE_LABEL:=release}
+export BASE_IMAGE_LABEL
+docker build --pull=true --build-arg BASE_IMAGE_LABEL=$BASE_IMAGE_LABEL -t sqlstream/streamlab-git:$BASE_IMAGE_LABEL .
+docker push sqlstream/streamlab-git:$BASE_IMAGE_LABEL
