24bbeeefb73a496518bc6050ac28c55374e93803
NigelThomas <nigel.cl.thomas@gmail.com>
2020-03-03 11:50:40 +0000

    add SQLSTREAM_DISABLE_PUMPS switch to allow container to start without starting pumps

--- develop-image/dockerrun.sh
+++ develop-image/dockerrun.sh
@@ -6,7 +6,11 @@
 # GIT_PROJECT_NAME : the namer of the git repo
 # CONTAINER_NAME : the name of the container as it is running
 # LOAD_SLAB_FILES : a space separated list of slab files (if not set, all will be loaded)
+
 # SQLSTREAM_HEAP_MEMORY : a value for the -Xmx parameter for s-Server (max heap space) default is 2048m
+# SQLSTREAM_DISABLE_PUMPS: set to non-blank to prevent pumps being started
+# SQLSTREAM_SLEEP_SECS: additional seconds to wait after s-Server started
+
 
 : ${BASE_IMAGE:=sqlstream/streamlab-git-dev}
 : ${GIT_ACCOUNT:=https://github.com/NigelThomas}
@@ -18,5 +22,6 @@ docker rm $CONTAINER_NAME
 
 docker pull $BASE_IMAGE
 docker run -p 80:80 -p 5560:5560 -p 5580:5580 -p 5585:5585 -p 5590:5590 -p 5595:5595 \
-            -e GIT_ACCOUNT=$GIT_ACCOUNT -e GIT_PROJECT_NAME=$GIT_PROJECT_NAME -e LOAD_SLAB_FILES="${LOAD_SLAB_FILES}" SQLSTREAM_HEAP_MEMORY="${SQSTREAM_HEAP_MEMORY}" \
+            -e GIT_ACCOUNT=$GIT_ACCOUNT -e GIT_PROJECT_NAME=$GIT_PROJECT_NAME -e LOAD_SLAB_FILES="${LOAD_SLAB_FILES}"  \
+            -e SQLSTREAM_HEAP_MEMORY="${SQLSTREAM_HEAP_MEMORY}" -e SQLSTREAM_DISABLE_PUMPS="$SQLSTREAM_DISABLE_PUMPS" -e SQLSTREAM_SLEEP_SECS=${SQLSTREAM_SLEEP_SECS} \
             -d --name $CONTAINER_NAME -it $BASE_IMAGE
