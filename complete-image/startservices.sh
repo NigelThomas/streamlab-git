1fd0bb73f9901d384c811f01c6a84706f18e78f1
NigelThomas <nigel.cl.thomas@gmail.com>
2020-03-20 05:59:40 +0000

    add startservices

---
+++ complete-image/startservices.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# If SQLSTREAM_JAVA_SECURITY_AUTH_LOGIN_CONFIG exist
+if [ ! -z "$SQLSTREAM_JAVA_SECURITY_AUTH_LOGIN_CONFIG" ]
+   then
+   echo "SQLSTREAM_JAVA_SECURITY_AUTH_LOGIN_CONFIG=$SQLSTREAM_JAVA_SECURITY_AUTH_LOGIN_CONFIG" >> /etc/sqlstream/environment
+fi
+
+# If SQLSTREAM_JAVA_SECURITY_KRB5_CONF exist
+if [ ! -z "$SQLSTREAM_JAVA_SECURITY_KRB5_CONF" ]
+   then
+   echo "SQLSTREAM_JAVA_SECURITY_KRB5_CONF=$SQLSTREAM_JAVA_SECURITY_KRB5_CONF" >> /etc/sqlstream/environment
+fi
+# s-server started by fetch_and_start_project
+service s-serverd start 
+service s-dashboardd start 
+service webagentd start
+service streamlabd start 
+service lighttpd start
+service postgresql start
+service kafka start
+source /etc/sqlstream/environment
+CP=$SQLSTREAM_HOME/../clienttools/blaze/coverpage-service/
+cd $SQLSTREAM_HOME/demo/coverpage/
+gulp build &>/dev/null
+service coverpaged start
