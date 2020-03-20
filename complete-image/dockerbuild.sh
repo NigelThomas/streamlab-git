d1c4b804aab8aae030270bfa9152d9d43217eb77
NigelThomas <nigel.cl.thomas@gmail.com>
2020-03-20 05:50:55 +0000

    don't pull automatically

--- complete-image/dockerbuild.sh
+++ complete-image/dockerbuild.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 : ${BASE_IMAGE_LABEL:=release}
 export BASE_IMAGE_LABEL
-docker build --pull=true --build-arg BASE_IMAGE_LABEL=$BASE_IMAGE_LABEL -t sqlstream/streamlab-git-complete:$BASE_IMAGE_LABEL .
+#docker build --pull=true --build-arg BASE_IMAGE_LABEL=$BASE_IMAGE_LABEL -t sqlstream/streamlab-git-complete:$BASE_IMAGE_LABEL .
+docker build --build-arg BASE_IMAGE_LABEL=$BASE_IMAGE_LABEL -t sqlstream/streamlab-git-complete:$BASE_IMAGE_LABEL .
 docker push sqlstream/streamlab-git-complete:${BASE_IMAGE_LABEL}
