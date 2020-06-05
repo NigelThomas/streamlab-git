78b3136d2b4b8c7765deeb58be6f7c8af9d57590
NigelThomas <nigel.cl.thomas@gmail.com>
2020-06-05 12:06:56 +0100

    use :latest tag for complete image as well

--- complete-image/dockerbuild.sh
+++ complete-image/dockerbuild.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-: ${BASE_IMAGE_LABEL:=release}
+: ${BASE_IMAGE_LABEL:=latest}
 export BASE_IMAGE_LABEL
 #docker build --pull=true --build-arg BASE_IMAGE_LABEL=$BASE_IMAGE_LABEL -t sqlstream/streamlab-git-complete:$BASE_IMAGE_LABEL .
 docker build --build-arg BASE_IMAGE_LABEL=$BASE_IMAGE_LABEL -t sqlstream/streamlab-git-complete:$BASE_IMAGE_LABEL .
